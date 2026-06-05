a75bf9971ebe6332a94a9175ed0e549a1c0dd0d2
vishalsolo21 <vishalsolo47@gmail.com>
2026-06-05 14:32:17 +0530

    Update check.js

--- truecaller/api/check.js
+++ truecaller/api/check.js
@@ -1,1 +1,27 @@
+export default async function handler(req, res) {
+  const { number } = req.query;
 
+  if (!number) {
+    return res.status(400).json({
+      success: false,
+      message: "Number is required"
+    });
+  }
+
+  try {
+    const response = await fetch(
+      `https://ye-lo-mojkro.noob73613.workers.dev/?api_key=@noob11001&number=${number}`
+    );
+
+    const data = await response.json();
+
+    res.setHeader("Access-Control-Allow-Origin", "*");
+    return res.status(200).json(data);
+
+  } catch (error) {
+    return res.status(500).json({
+      success: false,
+      message: error.message
+    });
+  }
+}
